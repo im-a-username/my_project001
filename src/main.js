743d49847d7d3455d938ae1b5d9117afad0ec82f
陈滔 <577840797@qq.com>
2019-07-07 15:44:30 +0800

    add files

--- src/main.js
+++ src/main.js
@@ -10,12 +10,13 @@ import './assets/css/global.css'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+    // 将axios 添加到原型中，那么vue实例可以直接通过 $http来访问axios
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
--- src/router.js
+++ src/router.js
@@ -4,28 +4,28 @@ import Login from './components/Login.vue'
 import Home from './components/Home.vue'
 
 Vue.use(Router)
-
+    // 路由规则
 const router = new Router({
-  routes: [
-    { path: '/', redirect: '/login' },
-    { path: '/login', component: Login },
-    { path: '/home', component: Home }
-  ]
+    routes: [
+        { path: '/', redirect: '/login' },
+        { path: '/login', component: Login },
+        { path: '/home', component: Home }
+    ]
 })
 
 //挂载路由导航守卫,to表示将要访问的路径，from表示从哪里来，next是下一个要做的操作
 router.beforeEach((to, from, next) => {
-  if (to.path === '/login')
-    return next();
+    if (to.path === '/login')
+        return next();
 
-  //获取token
-  const tokenStr = window.sessionStorage.getItem('token')
+    //获取token
+    const tokenStr = window.sessionStorage.getItem('token')
 
-  if (!tokenStr)
-    return next('/login')
+    if (!tokenStr)
+        return next('/login')
 
-  next();
+    next();
 
 })
 
-export default router 
+export default router
